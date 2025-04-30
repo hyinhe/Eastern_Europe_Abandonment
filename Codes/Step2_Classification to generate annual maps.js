06bac26f66cfeeb868fa90d7ee15ad8ca2514894
He Yin <hyinhe@gmail.com>
2025-04-30 11:03:47 -0400

    Update Step2_Classification to generate annual maps.js

--- Codes/Step2_Classification to generate annual maps.js
+++ Codes/Step2_Classification to generate annual maps.js
@@ -1,13 +1,12 @@
 /*Author:He Yin
   Date:21-Feb-2023 
-  Purpose: Generate land cover map for a certain year
-  Usage: To run the classification, first generate the spectral-temporal metrics with the “Calculate Spectral-Temporal Metrics from Landsat.js” script. Run the codes, then excuate "RUN" in the Tasks tab 
-  Parameters: outname; training_samples
+  Purpose: Generate land cover map for a certain year. The spectral-temporal metrics calculated from the Step-1 scirpt “Calculate Spectral-Temporal Metrics from Landsat.js” script are needed as inputs (line 15).
+  Usage: Run the codes, then excuate "RUN" in the Tasks tab 
+  Parameters: annualmetrics, outname; training_samples
 */
 
 var extent=ee.FeatureCollection('projects/ee-hyinhe/assets/He/SILVIS/Eastern_Europe/East_Europe_boundary_NUTS2_v3')
 
-Map.addLayer(extent,{})
 //zoom to the extent
 Map.centerObject(extent, 9);
 
@@ -21,22 +20,23 @@ var outdes='Class_'+annualmetrics_year.getInfo()
 var outname='EEurope_landcover/'+outdes+'_v1'
 print(annualmetrics_year)
 
-//Load the training_samples points generated in the previous step
+//Load the training_samples points. This dataset is also available in the folder "Data/Training_samples"
 var training_samples=ee.FeatureCollection('projects/ee-hyinhe/assets/He/SILVIS/Eastern_Europe/EEurope_training_samples').filterBounds(extent)
 
-// classify imagery
+// Sampling
 var training = annualmetrics.sampleRegions({
   collection: training_samples,
   properties: ['Class'],
   scale: 30
 });
-
+// Build a random forest classifier
 var classifier = ee.Classifier.randomForest(300).train({
   features: training, 
   classProperty: 'Class'
 });
 
-var classified = annualmetrics_dem.classify(classifier);
+classify imagery
+var classified = annualmetrics.classify(classifier);
 
 // Export the result to an Earth Engine asset.
 Export.image.toAsset({
